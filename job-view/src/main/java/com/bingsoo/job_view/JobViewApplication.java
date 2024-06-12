29b5e7022e8cc276c5989610343fdf294fc0c5ca
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-06-12 15:41:11 +0900

    view server, rest api server

---
+++ job-view/src/main/java/com/bingsoo/job_view/JobViewApplication.java
@@ -0,0 +1,13 @@
+package com.bingsoo.job_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobViewApplication.class, args);
+	}
+
+}
---
+++ job-view/src/test/java/com/bingsoo/job_view/JobViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingsoo.job_view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
