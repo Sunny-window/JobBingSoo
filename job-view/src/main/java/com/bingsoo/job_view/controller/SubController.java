2206e541999e064f41061d9e8468cb90e08d9be1
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-06-13 19:12:32 +0900

    헤더 구현중 푸터 구현

--- job-view/src/main/java/com/bingsoo/job_view/controller/SubController.java
+++ job-view/src/main/java/com/bingsoo/job_view/controller/SubController.java
@@ -1,5 +1,13 @@
 package com.bingsoo.job_view.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/sub")
+@Controller
 public class SubController {
-    
+    @RequestMapping("/")
+    public String sub(){
+        return "main/main";
+    }
 }
