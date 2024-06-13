1db4512e7870cf0ec35e7c511bbdd4f58ced7d08
DESKTOP-IH36R1G\user <hwi9158@gmail.com>
2024-06-13 11:30:49 +0900

    기업 컨트롤러, 뷰 초기설정

--- job-view/src/main/java/com/bingsoo/job_view/controller/IceController.java
+++ job-view/src/main/java/com/bingsoo/job_view/controller/IceController.java
@@ -1,5 +1,8 @@
 package com.bingsoo.job_view.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class IceController {
     
 }
--- job/src/main/java/com/bingsoo/job/controller/IceController.java
+++ job/src/main/java/com/bingsoo/job/controller/IceController.java
@@ -1,5 +1,10 @@
 package com.bingsoo.job.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ice")
 public class IceController {
     
 }
