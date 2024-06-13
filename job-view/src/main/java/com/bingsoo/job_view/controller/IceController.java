f6b977e30e0ffa024e1d111a7ffde2c06cdac1d9
DESKTOP-4DR04SQ\GG <c1004ds@naver.com>
2024-06-13 17:42:04 +0900

    개인/기업 알람 보내기 리스트 와 검색 창 개별 선택

--- job-view/src/main/java/com/bingsoo/job_view/controller/IceController.java
+++ job-view/src/main/java/com/bingsoo/job_view/controller/IceController.java
@@ -1,6 +1,7 @@
 package com.bingsoo.job_view.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/ice")
--- job/src/main/java/com/bingsoo/job/repository/CsRepository.java
+++ job/src/main/java/com/bingsoo/job/repository/CsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bingsoo.job.entity.Cs;
 
 public interface CsRepository extends JpaRepository<Cs, Long>{
-
+	
 }
