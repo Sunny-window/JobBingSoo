58452f6dad08fe03798e99b1d92c03df66d28722
osh <oshble5151@gmail.com>
2024-06-13 12:36:12 +0900

    mypage작성중 커밋

--- job-view/src/main/java/com/bingsoo/job_view/controller/RedbeanController.java
+++ job-view/src/main/java/com/bingsoo/job_view/controller/RedbeanController.java
@@ -1,5 +1,16 @@
 package com.bingsoo.job_view.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/red_bean")
 public class RedbeanController {
     
+	@RequestMapping("/mypage")
+	public String mypage() {
+		
+		return "red_bean/mypage";
+	}
 }
