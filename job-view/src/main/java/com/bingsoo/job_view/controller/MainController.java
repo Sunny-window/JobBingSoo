723481ed8b617aa944cfcf2961537677366e5145
Park Saehee <saehee0427@gmail.com>
2024-06-13 19:18:55 +0900

    개인 / 기업 회원가입 기능 작성

--- job-view/src/main/java/com/bingsoo/job_view/controller/MainController.java
+++ job-view/src/main/java/com/bingsoo/job_view/controller/MainController.java
@@ -1,5 +1,15 @@
 package com.bingsoo.job_view.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
 public class MainController {
     
+	@RequestMapping("/regForm")
+	public String regForm() {
+		
+		return "main/join_form";
+	}
 }
