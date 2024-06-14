ce9c5a4f86a7664541cbc5334511dcfe879c1e5f
Park Saehee <saehee0427@gmail.com>
2024-06-14 16:53:24 +0900

    red_bean / brith Date > LocalDate type 수정

---
+++ job/src/main/java/com/bingsoo/job/service/CompanyService.java
@@ -0,0 +1,24 @@
+package com.bingsoo.job.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bingsoo.job.entity.Company;
+import com.bingsoo.job.repository.CompanyRepository;
+
+@Service
+public class CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+    
+    public Company getCompanyByCno(String cno) {
+        return companyRepository.findByCno(cno).orElse(null);
+    }
+}
