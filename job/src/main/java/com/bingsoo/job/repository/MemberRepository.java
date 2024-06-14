5063c8d3f3ec570b84a90f61d930974a0bc28c4a
DESKTOP-4DR04SQ\GG <c1004ds@naver.com>
2024-06-14 19:40:03 +0900

    모달창 생성, 롤 기준으로 구분

--- job/src/main/java/com/bingsoo/job/repository/MemberRepository.java
+++ job/src/main/java/com/bingsoo/job/repository/MemberRepository.java
@@ -1,9 +1,15 @@
 package com.bingsoo.job.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bingsoo.job.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, String>{
+
+	List<Member> findByRole(String string);
+
+	Member findByUsername(String username);
 
 }
