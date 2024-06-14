a38faae06bdce9f7f8f7e6f6f86154cdc4ce9442
osh <oshble5151@gmail.com>
2024-06-14 17:53:41 +0900

    마이페이지, 레드빈 컨트롤러 수정후 커밋

--- job/src/main/java/com/bingsoo/job/controller/RedbeanController.java
+++ job/src/main/java/com/bingsoo/job/controller/RedbeanController.java
@@ -7,15 +7,23 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bingsoo.job.entity.Career;
+import com.bingsoo.job.entity.Certificate;
+import com.bingsoo.job.entity.Cover_letter;
+import com.bingsoo.job.entity.Desired_area;
+import com.bingsoo.job.entity.Desired_industry;
 import com.bingsoo.job.entity.Favorite;
-import com.bingsoo.job.entity.Member;
 import com.bingsoo.job.entity.RedBean;
 import com.bingsoo.job.entity.Resume;
 import com.bingsoo.job.entity.Subscribe;
+import com.bingsoo.job.repository.CareerRepository;
+import com.bingsoo.job.repository.CertificateRepository;
+import com.bingsoo.job.repository.Cover_letterRepository;
+import com.bingsoo.job.repository.Desired_areaRepository;
+import com.bingsoo.job.repository.Desired_industryRepository;
 import com.bingsoo.job.repository.FavoriteRepository;
 import com.bingsoo.job.repository.RedBeanRepository;
 import com.bingsoo.job.repository.ResumeRepository;
@@ -35,11 +43,43 @@ public class RedbeanController {
 	@Autowired
 	FavoriteRepository favoriteRepository;
 	
+	@Autowired
+	Cover_letterRepository cover_letterRepository;
+	
+	@Autowired
+	Desired_areaRepository desired_areaRepository;
+	
+	
+	@Autowired
+	Desired_industryRepository desired_industryRepository;
+	@Autowired
+	CareerRepository careerRepository;
+	@Autowired
+	CertificateRepository certificateRepository;
+	
 	
 	@PutMapping("/updateInfo")
 	public String updateInfo(@RequestBody RedBean redBean) {
 		System.out.println("==============================redBean : "+redBean);
 		redBeanRepository.save(redBean);
+		return "등록성공";
+	}
+
+	@PutMapping("/resumeUpdate")
+	public String resumeUpdate(@RequestBody Resume resume
+			,@RequestBody Cover_letter cover_letter
+			,@RequestBody Desired_area desired_area
+			,@RequestBody Desired_industry desired_industry
+			,@RequestBody Career career
+			,@RequestBody Certificate certificate
+			) {
+		System.out.println("==============================resume : "+resume);
+		resumeRepository.save(resume);
+		cover_letterRepository.save(cover_letter);
+		desired_areaRepository.save(desired_area);
+		desired_industryRepository.save(desired_industry);
+		careerRepository.save(career);
+		certificateRepository.save(certificate);
 		return "등록성공";
 	}
 	
