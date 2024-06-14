27bb2f4de7d8b786de3e17412767e2e490478792
Park Saehee <saehee0427@gmail.com>
2024-06-14 17:04:47 +0900

    누락 파일 업로드(Company Repository)

--- job/src/main/java/com/bingsoo/job/repository/CompanyRepository.java
+++ job/src/main/java/com/bingsoo/job/repository/CompanyRepository.java
@@ -1,5 +1,7 @@
 package com.bingsoo.job.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bingsoo.job.entity.Company;
@@ -8,4 +10,5 @@ import com.bingsoo.job.entity.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, String> {
 
+	Optional<Company> findByCno(String cno);
 }
