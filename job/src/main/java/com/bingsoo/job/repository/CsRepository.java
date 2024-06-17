0f18746161a58a4de016d6567a08a91af6963633
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-06-17 18:29:46 +0900

    헤더 관리자 로그인 링크 연결, cs repo method 추가

--- job/src/main/java/com/bingsoo/job/repository/CsRepository.java
+++ job/src/main/java/com/bingsoo/job/repository/CsRepository.java
@@ -2,7 +2,6 @@ package com.bingsoo.job.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +11,7 @@ import com.bingsoo.job.entity.Cs;
 public interface CsRepository extends JpaRepository<Cs, Long> {
 	@Query("SELECT c FROM Cs c WHERE c.type = :type ORDER BY CASE WHEN c.result = '미답변' THEN 0 ELSE 1 END, c.date ASC")
 	List<Cs> findByTypeOrderByResultAscDateAsc(@Param("type") String type);
+
+	@Query(value="SELECT * FROM cs WHERE public_type = '공개' ",nativeQuery = true)
+	List<Cs> findPublicAll();
 }
