c86d1e751dcfcf33681a61c3b20de6f92e2a2e15
DESKTOP-IH36R1G\user <hwi9158@gmail.com>
2024-06-18 17:13:15 +0900

    기업 알림 기능 구현

--- job/src/main/java/com/bingsoo/job/repository/SubscribeRepository.java
+++ job/src/main/java/com/bingsoo/job/repository/SubscribeRepository.java
@@ -1,9 +1,13 @@
 package com.bingsoo.job.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.bingsoo.job.entity.Member;
 import com.bingsoo.job.entity.Subscribe;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long>{
 
+    List<Subscribe> findByCid(Member cid);
 }
