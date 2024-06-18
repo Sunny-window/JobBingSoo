4bc89eca1e44e5630ab03e408e47676e0476f853
osh <oshble5151@gmail.com>
2024-06-18 19:13:28 +0900

    이력서 수정중 커밋

--- job/src/main/java/com/bingsoo/job/repository/Cover_letterRepository.java
+++ job/src/main/java/com/bingsoo/job/repository/Cover_letterRepository.java
@@ -1,9 +1,13 @@
 package com.bingsoo.job.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bingsoo.job.entity.Cover_letter;
 
 public interface Cover_letterRepository extends JpaRepository<Cover_letter, Long>{
-
+	
+    @Query(value = "SELECT * FROM cover_letter WHERE resume_code = :resume_code", nativeQuery = true)
+	Cover_letter findByResume_code(@Param("resume_code") long resume_code);
 }
