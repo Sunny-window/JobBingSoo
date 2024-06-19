58af5a2c2b36b26687d8adab1fde97120e15705d
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-06-19 12:31:59 +0900

    cs 디테일 완성, cs 등록 구현중. cs list 66%

--- job/src/main/java/com/bingsoo/job/controller/CsController.java
+++ job/src/main/java/com/bingsoo/job/controller/CsController.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bingsoo.job.entity.Cs;
 import com.bingsoo.job.entity.FAQ;
 import com.bingsoo.job.repository.CsRepository;
+import com.bingsoo.job.repository.Cs_replyRepository;
 import com.bingsoo.job.repository.FAQRepository;
 
 @RestController
@@ -23,6 +26,9 @@ public class CsController {
     @Autowired
     CsRepository cr;
 
+    @Autowired
+    Cs_replyRepository crrepo;
+
     @GetMapping("/faqlist")
     public List<FAQ> getFAQList(){
         return fr.findAll();
@@ -31,5 +37,11 @@ public class CsController {
     @GetMapping("/cslist")
     public List<Cs> getCsList(){
         return cr.findPublicAll();
+    }
+
+    @DeleteMapping("/cs")
+    public void deleteCs(@RequestParam("csCode")String code){
+        crrepo.deleteByCsCode(Long.parseLong(code));
+        cr.deleteById(Long.parseLong(code));
     }
 }
--- job/src/main/java/com/bingsoo/job/repository/Cs_replyRepository.java
+++ job/src/main/java/com/bingsoo/job/repository/Cs_replyRepository.java
@@ -3,10 +3,15 @@ package com.bingsoo.job.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bingsoo.job.entity.Cs;
 import com.bingsoo.job.entity.Cs_reply;
 
 public interface Cs_replyRepository extends JpaRepository<Cs_reply, Long> {
     List<Cs_reply> findByCsCode(Cs csCode);
+
+    @Query(value="DELETE FROM cs_reply WHERE cs_code = :code",nativeQuery=true)
+    void deleteByCsCode(@Param("code") long code);
 }
